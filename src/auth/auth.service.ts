59cba6aee2971bbedd5005749db1d47d93fad580
Ignacio Mendez Nole <ignaciomendeznole@gmail.com>
2024-03-24 23:17:47 -0300

    added placeholder services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
 
-  async registerUser(registerUserDto: any) {
+  async registerUser(registerUserDto: RegisterUserDto) {
     return this.natsClient.send('auth.register.user', registerUserDto);
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    return this.natsClient.send('auth.login.user', loginUserDto);
+  }
+
+  async verifyToken(token: string) {
+    return this.natsClient.send('auth.verify.user', token);
   }
 }
